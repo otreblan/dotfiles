d50efa408ef59c4559e8b51389b1799e1ef34ab6
otreblan <otreblain@gmail.com>
2019-06-05 14:22:39 -0500

    Now the replay script has the GPL notice

--- log/replay.sh
+++ log/replay.sh
@@ -2,6 +2,20 @@
 
 # A script to replay terminal recordings
 
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 LEVEL=0
 NEXTLEVEL=""
 DATE=""
@@ -10,8 +24,7 @@ SEPARATOR="-"
 until [[ $((LEVEL++)) -eq 4 ]]
 do
 	# Selecting the next level.
-	# It filters out the calling script,
-	# puts some the date in long format,
+	# It filters out the calling script, puts some the date in long format,
 	# and finally it only selects the last number.
 	NEXTLEVEL=$( \
 		ls | grep -v *.sh |\
@@ -31,10 +44,8 @@ do
 done
 
 # Selecting the replay to be played.
-# It filters the file extension
-# and deletes the duplicates,
-# also it puts the date in long format
-# and finally it´s filtered out.
+# It filters the file extension and deletes the duplicates,
+# also it puts and filters the date in long format..
 REPLAY=$(\
 	ls | sed "s/\(.*\)\..*/\1/g" |\
 	sed "s/\(.*\)/${DATE}\1/g" |\
