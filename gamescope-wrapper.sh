1a47a237625e209ee9e1856e5e8657701049bac0
otreblan <otreblain@gmail.com>
2026-07-03 21:26:04 -0500

    Fix slowdown at the 30 minute mark

--- gamescope-wrapper.sh
+++ gamescope-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gamescope -w 2560 -h 1440 -r 180 --fullscreen --adaptive-sync -- "$@"
+LD_PRELOAD="" exec gamescope -w 2560 -h 1440 -r 180 --expose-wayland --fullscreen --adaptive-sync -- env LD_PRELOAD="$LD_PRELOAD" "$@"
