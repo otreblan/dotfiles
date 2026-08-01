beb28082d2801378305a3ba5158c70187c09ad7a
otreblan <otreblain@gmail.com>
2026-07-31 23:03:39 -0500

    Less languages

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -149,7 +149,6 @@ local opts = {silent = true, noremap = true, expr = true, replace_keycodes = fal
 keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
 keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 
-
 require('impatient')
 
 -- Packer.nvim
@@ -161,7 +160,7 @@ require("nvim-treesitter.configs").setup {
 	},
 	highlight = {
 		enable  = true,
-		disable = {"latex", "lua", "cpp"},
+		disable = {"latex", "lua", "cpp", "markdown", "java"},
 	},
 	indent = {
 		enable = true,
